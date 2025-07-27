61c924377b6a8e36df6c7c7ed1bb7afa230acbef
전봉식 <bongsik@jeonbongsig-ui-MacBookAir.local>
2025-07-27 17:43:41 +0900

    Add router

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import TodoBoard from "../components/TodoBoard";
 import AddTaskForm from "../components/AddTaskForm";
 import { useState, useEffect } from "react";
 import { getTasks } from "../utils/api/tasks";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [todoList, setTodoList] = useState([]);
@@ -20,6 +21,9 @@ function Home() {
     <div className="w-full flex flex-col items-center h-screen bg-gray-300 p-5 sm:p-20 text-sm sm:text-base">
       <div className="w-full flex justify-between items-center max-w-2xl mb-2">
         <h1>ToDo App</h1>
+        <Link to="/login" className="text-blue-500 hover:text-blue-700">
+          Login
+        </Link>
       </div>
       <AddTaskForm setTodoList={setTodoList} />
       <TodoBoard todoList={todoList} setTodoList={setTodoList} />
